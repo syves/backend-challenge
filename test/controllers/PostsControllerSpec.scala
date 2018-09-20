7b069eb8fc2acb138de83c77680e38e5aded4b26
Shakrah Yves <7shakrahs@gmail.com>
2018-09-20 17:05:01 +0200

    move implicits to correct test scope

--- test/controllers/PostsControllerSpec.scala
+++ test/controllers/PostsControllerSpec.scala
@@ -18,11 +18,12 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class PostsControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
-
-  //val materializer = inject[ActorMaterializer]
   /*
   "PostsController POST/posts should" should {
-  val executionContext = inject[ExecutionContext]
+
+    val executionContext = inject[ExecutionContext]
+    implicit val sys = ActorSystem("MyTest")
+    implicit val materializer = ActorMaterializer()
 
     "render a single post on `create` page from a new instance of controller" in {
       //postRepository, executionContext
@@ -36,10 +37,6 @@ class PostsControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
       val create = controller.create(newPost).apply(FakeRequest(POST, "/posts"))
 
       status(create) mustBe OK
-      //TODO some("json") ?
-      //TODO create post
-      //TODO check for post after by Id
-      //TODO create delete view
 
       contentType(create) mustBe Some("application/json")
       contentAsString(create) must include ("Create Post")
@@ -49,21 +46,19 @@ class PostsControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
 
   "PostsController GET/posts/:id" should {
 
-    val executionContext = inject[ExecutionContext]
-    implicit val sys = ActorSystem("MyTest")
-    implicit val materializer = ActorMaterializer()
-
-
-
     "render a single post on `posting` page from a new instance of controller" in {
-      val controller = new PostsController(stubControllerComponents(),PostRepository(ec = executionContext),
+      val executionContext = inject[ExecutionContext]
+      implicit val sys = ActorSystem("MyTest")
+      implicit val materializer = ActorMaterializer()
+
+      val controller = new PostsController(
+        stubControllerComponents(),
+        PostRepository(ec = executionContext),
         executionContext)
-      val getById = controller.readSingle(1)().apply(FakeRequest(GET, "/posts/:id"))
+
+      val getById = controller.readSingle(id=1)().apply(FakeRequest(GET, "/posts/:id"))
 
       status(getById) mustBe OK
-      //TODO some("json")
-      //TODO create post
-      //TODO delete post after
       //TODO create posting view
 
       contentType(getById) mustBe Some("application/json")
@@ -72,16 +67,18 @@ class PostsControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
   }
 /*
   "PostsController GET/posts" should {
-    val executionContext = inject[ExecutionContext]
+
+
     "render all the posts on `list` page from a new instance of controller" in {
+
+    val executionContext = inject[ExecutionContext]
+    implicit val sys = ActorSystem("MyTest")
+    implicit val materializer = ActorMaterializer()
       val controller = new PostsController(stubControllerComponents(), PostRepository(ec = executionContext),
         executionContext)
       val posts = controller.readall().apply(FakeRequest(GET, "/posts"))
 
       status(posts) mustBe OK
-      //TODO some("json")
-      //TODO create posts
-      //TODO delete posts after
       //TODO count of posts
       //TODO extra credit pagination?
       contentType(posts) mustBe Some("application/json")
@@ -91,16 +88,18 @@ class PostsControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
   }
 
   "PostsController PUT/posts/:id" should {
-    val executionContext = inject[ExecutionContext]
+
+
     "render a single post on `update` page, after update, from a new instance of controller" in {
+        val executionContext = inject[ExecutionContext]
+    implicit val sys = ActorSystem("MyTest")
+    implicit val materializer = ActorMaterializer()
+
       val controller = new PostsController(stubControllerComponents(),PostRepository(ec = executionContext),
         executionContext)
       val updateById = controller.update(id: Int)().apply(FakeRequest(PUT, "/posts/:id"))
 
       status(updateById) mustBe OK
-      //TODO some("json")
-      //TODO create post
-      //TODO delete post after
       //TODO create update view
 
       contentType(updateById) mustBe Some("application/json")
@@ -109,16 +108,19 @@ class PostsControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injectin
   }
 
   "PostsController DELETE/posts/:id" should {
-    val executionContext = inject[ExecutionContext]
-    //todo check delete behavior..i think returns a message?
-    "render a single post? on `delete` page, after update, from a new instance of controller" in {
+
+
+    "render a single message on `delete` page, after update, from a new instance of controller" in {
+
+     val executionContext = inject[ExecutionContext]
+    implicit val sys = ActorSystem("MyTest")
+    implicit val materializer = ActorMaterializer()
       val controller = new PostsController(stubControllerComponents(), PostRepository(ec = executionContext),
         executionContext)
       val DeleteById = controller.delete(id: Int)().apply(FakeRequest(POST, "/posts/:id"))
 
       status(DeleteById) mustBe OK
-      //TODO some("json") ?
-      //TODO create post
+
       //TODO check for post after by Id
       //TODO create delete view
 
